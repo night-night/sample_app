8b56dddfb381ea6b2d79a2b6c520593a323c56ca
Aydar Kamaltdinov <aydar.kamaltdinov@gmail.com>
2017-06-18 00:29:17 +0300

    Cut redundant test in application_helper_test.rb

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,10 +4,6 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title,              "Ruby on Rails Sample App"
     assert_equal full_title("Help"),      "Help | Ruby on Rails Sample App"
-    assert_equal full_title("Home"),      "Home | Ruby on Rails Sample App"
-    assert_equal full_title("About"),     "About | Ruby on Rails Sample App"
-    assert_equal full_title("Contact"),   "Contact | Ruby on Rails Sample App"
-    assert_equal full_title("Sign up"),   "Sign up | Ruby on Rails Sample App"
   end
 
 end
